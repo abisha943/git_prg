41987cfbc085aecfd2a725b26335c2619aa662f1
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SL3N62.dir.svc.accenture.com>
2019-08-13 17:05:48 +0530

    commit1

---
+++ javademo/src/pack1/Calculator.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Calculator {
+public static void main(String[] args) {
+	int mark1=10;
+	int mark2=20;
+	int result=mark1+mark2;
+	System.out.println("The result is "+ result);
+}
+}
